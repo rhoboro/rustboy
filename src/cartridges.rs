850e0d8dfaac964e575dea2aa3565d05515facb0
rhoboro <rhoboro@gmail.com>
2021-12-26 12:44:03 +0900

    カートリッジヘッダーのEnumを定義

--- src/cartridges.rs
+++ src/cartridges.rs
@@ -21,11 +21,11 @@ struct CartridgeHeader {
     // 0146
     sgb_flag: u8,
     // 0147
-    cartridge_type: u8,
+    cartridge_type: CartridgeType,
     // 0148
-    rom_size: u8,
+    rom_size: RomSize,
     // 0149
-    ram_size: u8,
+    ram_size: RamSize,
     // 014A
     destination_code: u8,
     // 014B
@@ -38,8 +38,65 @@ struct CartridgeHeader {
     global_checksum: [u8; 2],
 }
 
-enum NewLicenseeCode {
-    None = 0x00,
+#[derive(Debug)]
+#[allow(dead_code)]
+enum CartridgeType {
+    RomOnly = 0x00,
+    Mbc1 = 0x01,
+    Mbc1Ram = 0x02,
+    Mbc1RamBattery = 0x03,
+    Mbc2 = 0x05,
+    Mbc2Battery = 0x06,
+    RomRam = 0x08,
+    RomRamBattery = 0x09,
+    Mmm01 = 0x0B,
+    Mmm01Ram = 0x0C,
+    Mmm01RamBattery = 0x0D,
+    Mbc3TimerBattery = 0x0F,
+    Mbc3TimerRamBatter = 0x10,
+    Mbc3 = 0x11,
+    Mbc3Ram = 0x12,
+    Mbc3RamBattery = 0x13,
+    Mbc5 = 0x19,
+    Mbc5Ram = 0x1A,
+    Mbc5RamBattery = 0x1B,
+    Mbc5Rumble = 0x1C,
+    Mbc5RumbleRam = 0x1D,
+    Mbc5RumbleRamBattery = 0x1E,
+    Mbc6 = 0x20,
+    Mbc7SensorRumbleRamZBattery = 0x22,
+    PocketCamera = 0xFC,
+    BandaiTama5 = 0xFD,
+    HuC3 = 0xFE,
+    HuC1RamBattery = 0xFF,
+}
+
+#[derive(Debug)]
+#[allow(dead_code)]
+enum RomSize {
+    KByte32 = 0x00,
+    KByte64 = 0x01,
+    KByte128 = 0x02,
+    KByte256 = 0x03,
+    KByte512 = 0x04,
+    MByte1 = 0x05,
+    MByte2 = 0x06,
+    MByte4 = 0x07,
+    MByte8 = 0x08,
+    MByte1_1 = 0x52,
+    MByte1_2 = 0x53,
+    MByte1_5 = 0x54,
+}
+
+#[derive(Debug)]
+#[allow(dead_code)]
+enum RamSize {
+    NoRam = 0x00,
+    UnUsed = 0x01,
+    KB8 = 0x02,
+    KB32 = 0x03,
+    KB128 = 0x04,
+    KB64 = 0x05,
 }
 
 #[derive(Debug)]
